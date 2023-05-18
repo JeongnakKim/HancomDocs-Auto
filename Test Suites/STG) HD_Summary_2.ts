ce356024aae259e036c59caea20f8cac6cebbf34
hancom <hancom@DESKTOP-PTKT1R2>
2023-05-18 16:02:57 +0900

    230518 : STG용 Test Suites Add

---
+++ Test Suites/STG) HD_Summary_2.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>STG) HD_Summary_2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>5c6f8ec4-f26b-4fae-85c8-0a1f9aa6ef10</testSuiteGuid>
+</TestSuiteEntity>
